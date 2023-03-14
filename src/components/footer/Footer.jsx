f1f00aa353eaf77b1adbf56a2bc183fb7ad85b5b
tomassalto <tomasagustinsalto@gmail.com>
2023-03-14 03:17:06 -0300

    change href

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,10 +7,10 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>TSALTO</a>
+      <a href="/#" className='footer__logo'>TSALTO</a>
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="/#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
